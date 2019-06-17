325c350e5ed4bdacc1f4839c04e928e7df72dfa2
Drainyyyy <videomoritz@gmail.com>
2019-06-18 00:01:00 +0200

    Added first login structure

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
-from flask import Flask
+from flask import Flask, redirect, render_template, request
 
 import config
 
 app = Flask(__name__)
+logged_in = False
 
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    print(request.base_url)
+    if logged_in:
+        return redirect('/chat')
+    else:
+        return redirect('/login')
+
+
+@app.route('/login')
+def sign():
+    return render_template("login.html", base_url=request.base_url)
+
+
+@app.route('/auth')
+def auth():
+    return
 
 
 if __name__ == '__main__':
