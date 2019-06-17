e14b985b21ed23605eb712819d9207b8f66f815d
Drainyyyy <Drainyyyy@users.noreply.github.com>
2019-06-17 15:25:07 +0200

    Adding config file

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask
 
+import config
+
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def index():
     return 'Hello World!'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(**config.FLASK_RUN_CONFIG)
---
+++ config.py
@@ -0,0 +1,5 @@
+FLASK_RUN_CONFIG = {
+    "host": "localhost",
+    "port": 1337,
+    "debug": True
+}
