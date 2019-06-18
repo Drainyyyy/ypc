45b822acb50d257e8fc728bf7429a3580851eb2f
Drainyyyy <videomoritz@gmail.com>
2019-06-18 23:31:31 +0200

    Continued with auth (not working)

--- utilities/auth.py
+++ utilities/auth.py
@@ -1,12 +1,24 @@
 from models.account import Account
 
+# TODO switch to Config instead of users dict
+
+users = {Account("Drainyyy", "xyz12345", 0)}
+
 
 class Auth:
     def __init__(self, account: Account):
         self.account = account
 
     def login(self):
-        print(self.account.username)
-        print(self.account.password)
-        print(self.account.admin)
-        return False
+        for acc in users:
+            print(acc is self.account)
+            print(acc.username)
+            print(acc.password)
+            print(acc.uid)
+            print(self.account.username)
+            print(self.account.password)
+            print(self.account.uid)     # TODO accounts not the same
+            if acc is self.account:
+                return True
+            else:
+                return False
