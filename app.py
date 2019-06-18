130ed24ae40c67aaf0ffdafbf3754a3db6c8cd0a
Drainyyyy <videomoritz@gmail.com>
2019-06-18 14:32:23 +0200

    Added auth route and edited login route

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, redirect, render_template, request
+from werkzeug.exceptions import BadRequestKeyError
 
 import config
+from models.account import Account
+from utilities.auth import Auth
 
 app = Flask(__name__)
 logged_in = False
 
-
+# TODO Copyright!
 @app.route('/')
 def index():
     print(request.base_url)
@@ -15,14 +18,24 @@ def index():
         return redirect('/login')
 
 
-@app.route('/login')
+@app.route('/login', methods=["POST", "GET"])
 def sign():
-    return render_template("login.html", base_url=request.base_url)
+    print(request.url_root)
+    return render_template("login.html", base_url=request.url_root)
 
 
-@app.route('/auth')
+@app.route('/auth', methods=["POST", "GET"])
 def auth():
-    return
+    try:
+        _username = request.form["username"]
+        _password = request.form["password"]
+        _auth = Auth(Account(_username, _password)).login()
+        if _auth:
+            return redirect("/chat")
+        else:
+            return redirect("/login")
+    except BadRequestKeyError:
+        return redirect("/login")
 
 
 if __name__ == '__main__':
