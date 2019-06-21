98310cdaaf636b08cdfdca18675bab515a72d4b6
Drainyyyy <videomoritz@gmail.com>
2019-06-21 14:32:44 +0200

    Added file encoding comment

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 #  Copyright (c) 2019. Drainyyy
 #  This project is covered by MIT License
 #  https://opensource.org/licenses/MIT
--- utilities/accmanager.py
+++ utilities/accmanager.py
@@ -1,6 +1,9 @@
+# -*- coding: utf-8 -*-
+
 #  Copyright (c) 2019. Drainyyy
 #  This project is covered by MIT License
 #  https://opensource.org/licenses/MIT
+
 
 def uid():
     return 0
