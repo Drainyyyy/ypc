c044f3458af1bd64042a228914a2f733a1573588
Drainyyyy <videomoritz@gmail.com>
2019-07-07 20:12:37 +0200

    Finished Message model

--- ypc/models/message.py
+++ ypc/models/message.py
@@ -6,19 +6,45 @@
 
 from datetime import datetime
 
-from ypc.models.account import DefaultAccount, AdminAccount
-from ypc.utilities.auth import Auth
+from .enums import MessageType
+from .account import Account
 from ypc.utilities.formatter import timestamp_for_message
 
-__all__ = ("MemberMessage", "ManageMessage")
+__all__ = ["Message"]
 
 
-class MemberMessage:
+class Message:
+    """The model for every type of message that is sent to the chat.
 
-    def __init__(self, author: DefaultAccount, timestamp: datetime, message_id: int, pinned: bool, content: str):
+    :param author: self-explaining
+    :param timestamp: A timestamp for when the message was sent to the chat.
+    :param message_id: The unique identifier of the corresponding message.
+    :param pinned: Marks if the message is in the list of pinned messages. This is similar to saving a message. Messages can only be pinned by admins.
+    :param content: self-explaining
+    :param message_type: See :func:`MessageType <ypc.models.enums.MessageType>`
+    """
+
+    __slots__ = ["author", "sent_at", "id", "pinned", "content", "mentions", "type"]
+
+    def __init__(self, author: Account, timestamp: datetime, message_id: int, pinned: bool, content: str, message_type: MessageType):
         self.author = author
-        self.created_at = timestamp_for_message(timestamp)
-        self.id = message_id    # TODO automated id
+        self.sent_at = timestamp_for_message(timestamp)
+        self.id = message_id
         self.pinned = pinned
         self.content = content
         self.mentions = None    # TODO mentions
+        self.type = message_type
+
+    def __str__(self):
+        return self.content
+
+    def __repr__(self):
+        representer = "<ypc.Message author={} send_at={} message_id={} pinned={} content={} message_type={}".format(
+            self.author,
+            self.sent_at,
+            self.id,
+            self.pinned,
+            self.content,
+            self.type
+        )
+        return representer
