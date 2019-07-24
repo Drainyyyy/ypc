626b1a6c20b24b81c184c33d1e490f15e065a132
Drainyyyy <videomoritz@gmail.com>
2019-07-24 16:35:50 +0200

    Finished Account Exceptions except documentation

--- ypc/api/errors/account_exceptions.py
+++ ypc/api/errors/account_exceptions.py
@@ -30,14 +30,13 @@ class AccountNotFoundException(AccountException):
 
             super(AccountException, self).__init__(*args if args is not None else message, account.__repr__())
         else:
-            message = "The requested account was not found."
-
-            super().__init__(*args if args is not None else message)
+            pass
 
 
 class AccountNotPermittedException(AccountException):
     """This exception is thrown whenever an account tries to do something it is not allowed to.
-    TODO documentation
+
+    Thrown for example if a member tries to ban a member with a higher rank.
     """
 
     def __init__(self, account: Account, *args):
@@ -54,7 +53,7 @@ class AccountNotPermittedException(AccountException):
         }
 
         self.type = types[account.type]
-        self.status = status[account.status]
+        self.status = status[account.state]
 
         message = "The account has not enough permissions for the requested action."
 
@@ -64,6 +63,24 @@ class AccountNotPermittedException(AccountException):
 class AccountBannedException(AccountException):
     """Thrown when an account is banned but want's to perform an action but is not allowed to because it's banned.
 
-    This can be thrown for example when the account tries to login, but is banned. TODO finish
+    Can be thrown for instance when the account tries to login while being banned.
     """
 
+    pass
+
+
+class AccountMutedException(AccountException):
+    """
+TODO docs
+    """
+
+    pass
+
+
+class AccountOfflineException(AccountException):
+    """
+
+TODO docs
+    """
+
+    pass
