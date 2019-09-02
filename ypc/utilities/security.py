b4e2c773fdc3e22c9ca86d6b06c29048885c2f8a
Drainyyyy <videomoritz@gmail.com>
2019-09-02 23:34:49 +0200

    Renamed pwhandler to security because of encrypten and hashing of everything in this file

---
+++ ypc/utilities/security.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+#  Copyright (c) 2019. Drainyyy
+#  This project is covered by MIT License
+#  https://opensource.org/licenses/MIT
+
+
+class Encryption:   # TODO rename to hash
+
+    @staticmethod
+    def encrypt(password):
+        print("Encryption not finished!")
+        return password
