263305f104fc3a83163e8693e945efd0fd4af0c4
Drainyyyy <videomoritz@gmail.com>
2019-09-02 23:37:48 +0200

    added documentation to timestamp_for_message formatter

--- ypc/utilities/formatter.py
+++ ypc/utilities/formatter.py
@@ -4,9 +4,19 @@
 #  This project is covered by MIT License
 #  https://opensource.org/licenses/MIT
 
-import datetime
+from datetime import datetime
 
-# TODO documentation!!!
-def timestamp_for_message(timestamp: datetime.datetime):
-    formatted = timestamp.strftime("%Y-%m-%d ~ %H:%M")
+
+def timestamp_for_message(timestamp: datetime):
+    """A formatter that formats a timestamp into a special format.
+
+    The timestamp get's formatted into "day-month-year ~ hour:minute".
+    Example: The "15th August 2019 22:08 O'Clock" would be "15-8-2019 ~ 22:08"
+
+    :param timestamp:
+    :type timestamp: datetime
+    :return:
+    """
+
+    formatted = timestamp.strftime("%d-%m-%Y ~ %H:%M")
     return formatted
