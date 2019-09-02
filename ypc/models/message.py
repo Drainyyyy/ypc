c181f66a34bc3a8c96158e12112e423d1f0241e4
Drainyyyy <videomoritz@gmail.com>
2019-09-02 23:36:49 +0200

    Added embed model

--- ypc/models/message.py
+++ ypc/models/message.py
@@ -6,11 +6,12 @@
 
 from datetime import datetime
 
+from ..utilities.color import Color
 from .enums import MessageType
 from .client import Client
-from ypc.utilities.formatter import timestamp_for_message
+from ..utilities.formatter import timestamp_for_message
 
-__all__ = ["Message", "Embed"]  # TODO make embed
+__all__ = ["Message", "Embed"]
 
 
 class Message:
@@ -26,12 +27,12 @@ class Message:
 
     __slots__ = ["author", "sent_at", "id", "pinned", "content", "mentions", "type"]
 
-    def __init__(self, author: Client, timestamp: datetime, message_id: int, pinned: bool, content: str, message_type: MessageType):
+    def __init__(self, author: Client, content: str, timestamp: datetime, message_id: int, pinned: bool, message_type: MessageType):
         self.author = author
+        self.content = content
         self.sent_at = timestamp_for_message(timestamp)
         self.id = message_id
         self.pinned = pinned
-        self.content = content
         self.mentions = None    # TODO mentions
         self.type = message_type
 
@@ -39,12 +40,50 @@ class Message:
         return self.content
 
     def __repr__(self):
-        representation = "<ypc.Message author={} send_at={} message_id={} pinned={} content={} message_type={}>".format(
+        representation = "<ypc.Message author={} content={} timestamp={} message_id={} pinned={} mentions={} message_type={}>".format(
             self.author,
+            self.content,
             self.sent_at,
             self.id,
             self.pinned,
-            self.content,
+            self.mentions,
             self.type
         )
+
         return representation
+
+
+class Embed(Message):
+    """A message with a special border on it. Also this message type has an addition title.
+
+    Usable to mark special or important messages.
+    """
+
+    __slots__ = super().__slots__ + ["color", "title"]
+
+    def __init__(self, author: Client, content: str, timestamp: datetime, message_id: int, pinned: bool, message_type: MessageType,
+                 border_color: Color, title: str = None):
+        super().__init__(author, content, timestamp, message_id, pinned, message_type)
+
+        self.color = border_color
+        self.title = title
+
+    def __str__(self):
+        return self.content
+
+    def __repr__(self):
+        representation = "<ypc.Embed author={} content={} timestamp={} message_id={} pinned={} message_type={} border_color={} title={}>".format(
+            self.author,
+            self.content,
+            self.sent_at,
+            self.id,
+            self.pinned,
+            self.type,
+            self.color,
+            self.title
+        )
+
+        return representation
+
+
+
